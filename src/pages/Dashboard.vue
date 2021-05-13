43377549eca0efc1390d60d4e9c20005a429ade3
lixiang <lixiangyuanyang@163.com>
2021-05-13 17:15:35 +0800

    Dashboard

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="description">
+      Vue2.x学习笔记
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Dashboard"
+}
+</script>
+
+<style scoped>
+
+</style>
