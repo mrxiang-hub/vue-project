1f377b20180b801b30d7bf47e3eb109fb36a90ce
lixiang <lixiangyuanyang@163.com>
2021-05-14 10:00:22 +0800

    slot 插槽

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id="app">
-      <Common></Common>
+    <Common></Common>
+
+    <Card>
+      <template v-slot:header>
+        <div>我是头部</div>
+      </template>
+      <template v-slot:center>
+        <div>我是中间</div>
+      </template>
+      <template v-slot:footer>
+        <div>我是脚部</div>
+      </template>
+    </Card>
   </div>
 </template>
 
 <script>
+import Card from "@/components/card";
 
 export default {
   name: 'App',
-
+  components: {
+    Card
+  }
 }
 </script>
 
---
+++ src/components/card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <slot name="header"></slot>
+    <slot name="center"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card"
+}
+</script>
+
+<style scoped>
+
+</style>
