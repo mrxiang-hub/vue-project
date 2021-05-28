6f67a2b98df1d8ce40d623eba12076a0089e2431
lixiang <lixiangyuanyang@163.com>
2021-05-28 12:04:06 +0800

    上传代码

--- src/views/keepAlive/keepAlive.vue
+++ src/views/keepAlive/keepAlive.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="alive">
+    <!--    // 什么是keepAlive-->
     <el-card>
       <div slot="header" class="clearfix">
         <span>keepAlive</span>
@@ -23,7 +24,19 @@
       >
       </el-alert>
     </el-card>
-
+    <!--    如何实现keepAlive-->
+    <el-card>
+      <div slot="header" class="clearfix">
+        <span>实现路由缓存的几种方法</span>
+      </div>
+    </el-card>
+    <div class="item" v-for="(item,index) in application" :key="index">
+      <h3 class="title">{{ item.title }}</h3>
+      <div clsss="desc" v-for="(n,key) in item.desc"
+           :key="key"
+           v-html="n"
+      ></div>
+    </div>
   </div>
 </template>
 
@@ -48,6 +61,15 @@ export default {
             'exclude : 排除在外的组件name 数组形式[A, B]',
             'max: 缓存组件的最大值, number类型'
           ],
+        }
+      ],
+      application: [
+        {
+          title: '1： 利用路由的meta属性',
+          desc: ['1：首先在路由的meta对象中添加 keepAlive： true键值对',
+            '2:写两个router-view标签, 在需要缓存的视图标签外包裹 keep-alive标签',
+            '如图:<img src="@/assets/img/keep.png"/>'
+          ]
         }
       ]
     }
