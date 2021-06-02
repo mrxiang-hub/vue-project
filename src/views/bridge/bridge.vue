d5ae633e38715bd4e2dc3e05ea1d6faa4412cf8a
lixiang <lixiangyuanyang@163.com>
2021-06-02 15:18:22 +0800

    双向绑定

--- src/views/bridge/bridge.vue
+++ src/views/bridge/bridge.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    
+  <div class="bridge">
+
   </div>
 </template>
 
@@ -11,5 +11,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.bridge {
+  height: 100%;
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
