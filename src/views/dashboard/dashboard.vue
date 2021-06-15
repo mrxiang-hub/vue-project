43676bf9fd66b1e05ff3922519e3b4465320e263
lixiang <lixiangyuanyang@163.com>
2021-06-15 15:37:26 +0800

    代码恢复

--- src/views/dashboard/dashboard.vue
+++ src/views/dashboard/dashboard.vue
@@ -1,28 +1,16 @@
 <template>
   <div>
-    <Simple v-model="val"/>
-    <button @click="getData">点击</button>
+    Vue学习笔记
   </div>
 </template>
 
 <script>
-import Simple from './simple';
 
 export default {
   name: "Dashboard",
-  components: {
-    Simple
-  },
   data() {
-    return {
-      val: ''
-    }
+    return {}
   },
-  methods: {
-    getData() {
-      console.log(this.val);
-    }
-  }
 }
 </script>
 
