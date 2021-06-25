d763db49dd95ff078691ebdbc29bf5814528c17c
lixiang <lixiangyuanyang@163.com>
2021-06-25 15:57:23 +0800

    混入完成

--- src/views/mix/example2.vue
+++ src/views/mix/example2.vue
@@ -1,16 +1,29 @@
 <template>
-  <div>
-    混入2
+  <div class="example2">
+    <el-button type="primary" @click="showModal">显示</el-button>
+    <el-button type="danger" @click="closeModal">隐藏</el-button>
+    <div class="box" v-if="isShow">
+
+    </div>
   </div>
 </template>
 
 <script>
-// import myMixin from "@/views/mix/myMixin";
+import myMixin from "@/views/mix/myMixin";
+
 export default {
-  name: "example2"
+  name: "example2",
+  mixins: [myMixin],
+  data() {
+    return {}
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.box {
+  width: 100px;
+  height: 100px;
+  background: #F56C6C;
+}
 </style>
